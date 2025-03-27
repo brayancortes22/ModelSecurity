751b73c5611a2c0e7e6510ca67931212b9b5da0e
bscl <brayanstidcorteslombana@gmail.com>
2025-03-26 23:54:22 -0500

    Agregar archivos de proyecto.

---
+++ Diagram2/DbAutogestion1.Designer.cs
@@ -0,0 +1,2 @@
+﻿// Ver este comentario implica un error en la generación de código del archivo 'C:\Users\bscl\source\repos\ModelSecurity\Diagram2\DbAutogestion.edmx'
+//. Vea ErrorList para obtener detalles.
---
+++ Entity/Context/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Dapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Reflection;
+using Entity.Model;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics.Contracts;
+using System.Runtime.InteropServices;
+
+namespace Entity.Context
+{
+    internal class ApplicationDbContext
+    {
+        /// <summary>
+
+        /// Actualiza un rol existente en la base de datos.
+
+        /// </summary>
+
+        /// <param name="rol">Objeto con la información actualizada. </param>
+
+        /// <returns>True si la operación fue exitosa, False en caso contrario.</returns>
+        public async Task<bool> UpdateAsync(Rol rol)
+        {
+            try
+            {
+                _context.Set<Rol>().Update(rol);
+
+                await _context.SaveChangesAsync();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error al actualizar el rol: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
